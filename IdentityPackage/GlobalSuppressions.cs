d7f61219fb4c8d610111b898096085072520704a
Ruan de Jongh <70750945+Desolate1998@users.noreply.github.com>
2023-03-01 10:11:36 +0200

    Delete GlobalSuppressions.cs

--- IdentityPackage/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Critical Code Smell", "S2223:Non-constant static fields should not be visible", Justification = "Need it to be public so the password rules can be set", Scope = "member", Target = "~F:IdentityPackage.IdentitityInternalServices.PasswordService.options")]
